e2d01d0a502aa9215c18045dd85ebab73b6a0d03
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-27 06:42:28 +0530

    Update __init__.py

--- jane/__init__.py
+++ jane/__init__.py
@@ -10,20 +10,14 @@ from pyrogram import Client, errors
 from telethon import TelegramClient
 import config 
 
-
 # enable logging
 logging.basicConfig(
-    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     handlers=[logging.FileHandler("log.txt"), logging.StreamHandler()],
     level=logging.INFO,
 )
 
-logging.getLogger("apscheduler").setLevel(logging.ERROR)
-logging.getLogger("telethon").setLevel(logging.ERROR)
-logging.getLogger("pyrogram").setLevel(logging.ERROR)
 LOGGER = logging.getLogger(__name__)
-
-
 
 import config 
 
