8f231ec890102def0214d7eb253c0eaffbbb7b1c
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-27 22:16:35 +0530

    Update zombies.py

--- jane/modules/zombies.py
+++ jane/modules/zombies.py
@@ -54,7 +54,7 @@ __help__ = """
 *Remove Deleted Accounts*
 
  ❍ /zombies *:* Starts searching for deleted accounts in the group.
- ❍ /zombies clean *:* Removes the deleted accounts from the group.
+ ❍ `/zombies clean` *:* Removes the deleted accounts from the group.
 """
 
 __mod_name__ = "Zᴏᴍʙɪᴇ"
