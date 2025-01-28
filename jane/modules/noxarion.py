be086d96d23ab5e74d7e23d340fffe767e4adb6e
Anjlnobita <jiteshkumar7200@gmail.com>
2025-01-29 02:15:09 +0530

    Update noxarion.py

--- jane/modules/noxarion.py
+++ jane/modules/noxarion.py
@@ -2,7 +2,6 @@
 
 from telethon import events, functions
 from telethon.tl.types import ChatAdminRights
-from telethon.errors import UserNotParticipant
 from jane import telethn
 
 @telethn.on(events.NewMessage(pattern=f"^[!/]promote ?(.*)"))
@@ -11,18 +10,14 @@ async def promote(event):
     user = await event.get_reply_message()
     if user:
         user_id = user.from_id
-        try:
-            user_member = await chat.get_member(user_id)
-        except UserNotParticipant:
-            await event.reply("» The user is not a member of this chat.")
-            return
-        if user_member.status in ("administrator", "creator"):
-            await event.reply("» According to me that user is already an admin here!")
-            return
         if user_id == telethn.me.id:
             await event.reply("» I can't promote myself.")
             return
         try:
+            user_member = await chat.get_member(user_id)
+            if user_member.status in ("administrator", "creator"):
+                await event.reply("» According to me that user is already an admin here!")
+                return
             await telethn.edit_admin(
                 chat_id=chat.id,
                 user_id=user_id,
