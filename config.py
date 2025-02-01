d170e8074201438196b60bbd9130c05d546306a6
Anjlnobita <jiteshkumar7200@gmail.com>
2025-02-01 17:37:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,12 @@
 import re
-import sys
 from os import getenv
 
-
+from dotenv import load_dotenv
 from pyrogram import filters
 
+load_dotenv()
 
 
-API_ID = 20650066
-API_HASH = "7a4f8ed638f1369a40693574c2835217"
-
-BOT_TOKEN = ""
 
 
 MONGO_DB_URI = "mongodb+srv://anjlnobita:tCUPU9Ty1FFvLumv@cluster0.appf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -35,16 +31,11 @@ WOLVES = 7335060704
 
 
 
-SUPPORT_CHANNEL = "https://t.me/anjlnobita"
-SUPPORT_CHAT = "https://t.me/anime_societyy"
-
-
-
-AI_API_KEY = "awoo"
+AI_API_KEY = "api key "
 
 LOG = 2
 LOG_FILE_NAME = "logs.txt"
-TEMP_DB_FOLDER = "anjlnobita"
+TEMP_DB_FOLDER = "jane_doe"
 
 
 EVENT_LOGS = ()  
@@ -69,6 +60,132 @@ URL = None
 
 BAN_STICKER = "" 
 
-START_IMG = "https://telegra.ph/file/ee28ffa1fa36ac19faa41.jpg"
 
+
+
+DRAGONS = 6777860063
+DEV_USERS = 7335060704
+DEMONS = 6397808634
+MEOW = 7666460878
+
+#OWNER_ID = [DEV_USERS, DRAGONS, DEMONS, MEOW]
+
+
+OWNER_ID = (6777860063, 7335060704, 6397808634, 7666460878)
+
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 888))
+
+
+
+
+
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/anjlnobita/jane_doe",
+)
+
+
+
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+) 
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/noxarion_network")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/anime_societyy")
+
+
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+
+
+
+
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+
+
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+
+
+
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
+
+
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+
+START_IMG_URL = getenv(
+    "START_IMG_URL", "https://envs.sh/kRs.jpg"
+)
+
+PING_IMG_URL = getenv(
+    "PING_IMG_URL", "https://envs.sh/JKS.jpg"
+)
+
+PLAYLIST_IMG_URL = "https://envs.sh/oX0.jpg"
+
+STATS_IMG_URL = "https://telegra.ph/file/d30d11c4365c025c25e3e.jpg"
+
+TELEGRAM_AUDIO_URL = "https://envs.sh/r78.jpg"
+
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e575ae40d6635250974e1.jpg"
+
+STREAM_IMG_URL = "https://telegra.ph/file/03efec694e41e891b29dc.jpg"
+
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+
+YOUTUBE_IMG_URL = "https://telegra.ph/file/4dc854f961cd3ce46899b.jpg"
+
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/d723f4c80da157fca1678.jpg"
+
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/6c741a6bc1e1663ac96fc.jpg"
+
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_GROUP:
+    if not re.match("(?:http|https)://", SUPPORT_GROUP):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
+        )
     
