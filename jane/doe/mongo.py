3be3eb8452dbceead5c9eb9bc0bdbc9dc1647896
Anjlnobita <jiteshkumar7200@gmail.com>
2025-02-01 22:18:49 +0530

    Update mongo.py

--- jane/doe/mongo.py
+++ jane/doe/mongo.py
@@ -1,24 +1,14 @@
-import asyncio
-import sys
+from motor.motor_asyncio import AsyncIOMotorClient
 
-from motor import motor_asyncio
-from jane import MONGO_DB_URI 
-from pymongo import MongoClient
-from pymongo.errors import ServerSelectionTimeoutError
-from jane.conf import get_int_key, get_str_key
+from config import MONGO_DB_URI
 
+from jane import LOGGER
 
-MONGO_PORT = get_int_key("27017")
-MONGO_DB_URI = get_str_key("MONGO_DB_URI")
-MONGO_DB = "nobii"
-
-
-client = MongoClient()
-client = MongoClient(MONGO_DB_URI, MONGO_PORT)[MONGO_DB]
-motor = motor_asyncio.AsyncIOMotorClient(MONGO_DB_URI, MONGO_PORT)
-db = motor[MONGO_DB]
-db = client["jane_doe"]
+LOGGER(__name__).info("Connecting to your Mongo Database...")
 try:
-    asyncio.get_event_loop().run_until_complete(motor.server_info())
-except ServerSelectionTimeoutError:
-    sys.exit(log.critical("Can't connect to mongodb! Exiting..."))
+    _mongo_async_ = AsyncIOMotorClient(MONGO_DB_URI)
+    mongodb = _mongo_async_.Anon
+    LOGGER(__name__).info("Connected to your Mongo Database.")
+except:
+    LOGGER(__name__).error("Failed to connect to your Mongo Database.")
+    exit()
