a2b70e3bbc761f754180ab5747cceda3a51c4271
Anjlnobita <jiteshkumar7200@gmail.com>
2025-02-02 17:19:07 +0530

    Update __init__.py

--- jane/__init__.py
+++ jane/__init__.py
@@ -27,26 +27,53 @@ LOGGER = logging.getLogger(__name__)
 
 
 
-import config 
-
-ALLOW_CHATS = config.ALLOW_CHATS
-ALLOW_EXCL = config.ALLOW_EXCL
-DB_URI = config.DATABASE_URL
-DEL_CMDS = config.DEL_CMDS
-EVENT_LOGS = config.EVENT_LOGS
-INFOPIC = config.INFOPIC
-#LOAD = config.LOAD
-MONGO_DB_URI = config.MONGO_DB_URI
-NO_LOAD = config.NO_LOAD
-START_IMG = config.START_IMG
-STRICT_GBAN = config.STRICT_GBAN
-SUPPORT_CHAT = config.SUPPORT_CHAT
-BAN_STICKER = config.BAN_STICKER
-AI_API_KEY = config.AI_API_KEY
-LOG_GROUP_ID = config.LOG_GROUP_ID
 
 
 
+
+
+
+
+
+MONGO_DB_URI = "mongodb+srv://anjlnobita:tCUPU9Ty1FFvLumv@cluster0.appf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+DATABASE_URL = "mongodb+srv://anjlnobita:tCUPU9Ty1FFvLumv@cluster0.appf0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+
+
+JOIN_LOGGER = -1002372313866
+LOG_GROUP_ID = -1002372313866
+EVENT_LOGS = -1002372313866
+
+
+OWNER_ID = 7335060704
+
+
+DRAGONS = 7335060704
+DEV_USERS = 7335060704
+DEMONS = 7335060704
+TIGERS = 7335060704
+WOLVES = 7335060704
+
+
+DRAGONS = 6777860063
+DEV_USERS = 7335060704
+DEMONS = 6397808634
+MEOW = 7666460878
+
+#OWNER_ID = [DEV_USERS, DRAGONS, DEMONS, MEOW]
+
+
+OWNER_ID = (6777860063, 7335060704, 6397808634, 7666460878)
+
+
+
+
+AI_API_KEY = "api key "
+
+LOG = 2
+LOG_FILE_NAME = "logs.txt"
+TEMP_DB_FOLDER = "jane_doe"
 
 
 EVENT_LOGS = ()  
@@ -69,34 +96,73 @@ WORKERS = 8
 WEBHOOK = False
 URL = None
 
+BAN_STICKER = "" 
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+
+
+
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/noxarion_network")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/anime_societyy")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 
 try:
-    OWNER_ID = int(config.OWNER_ID)
+    OWNER_ID = int(OWNER_ID)
 except ValueError:
     raise Exception("Your OWNER_ID variable is not a valid integer.")
 
 
 try:
     DRAGONS = set(int(x) for x in config.DRAGONS or [])
-    DEV_USERS = set(int(x) for x in config.DEV_USERS or [])
+    DEV_USERS = set(int(x) for x in DEV_USERS or [])
 except ValueError:
     raise Exception("Your sudo or dev users list does not contain valid integers.")
 
 try:
-    DEMONS = set(int(x) for x in config.DEMONS or [])
+    DEMONS = set(int(x) for x in DEMONS or [])
 except ValueError:
     raise Exception("Your support users list does not contain valid integers.")
 
 try:
-    WOLVES = set(int(x) for x in config.WOLVES or [])
+    WOLVES = set(int(x) for x in WOLVES or [])
 except ValueError:
     raise Exception("Your whitelisted users list does not contain valid integers.")
 
 try:
-    TIGERS = set(int(x) for x in config.TIGERS or [])
+    TIGERS = set(int(x) for x in TIGERS or [])
 except ValueError:
     raise Exception("Your tiger users list does not contain valid integers.")
 
