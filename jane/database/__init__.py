391fcdbc816bb9bce1c143d9c4ccc55b172d2a22
Anjlnobita <jiteshkumar7200@gmail.com>
2025-02-03 15:27:31 +0530

    Update __init__.py

--- jane/database/__init__.py
+++ jane/database/__init__.py
@@ -1,1 +1,17 @@
-#
+from pymongo import MongoClient
+from config import DB_URI
+from jane.logging import LOGGER as log
+
+def start():
+    client = MongoClient(DB_URI)
+    log.info("[MongoDB] Connecting to database......")
+    return client
+
+try:
+    client = start()
+    nobita = client.get_default_database()
+except Exception as e:
+    log.exception(f"[MongoDB] Failed to connect due to {e}")
+    exit()
+
+log.info("[MongoDB] Connection successful, client started.")
